8628281a36f27bac1764c85f93b31398e7bf72ab
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-06-21 17:47:21 -0300

    country services created

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,12 +1,22 @@
-
+import { useEffect, useState } from "react";
+import countryServices from "./services/countries";
 
 function App() {
+  const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    countryServices.getAll().then((initialPersons) => {
+      setCountries(initialPersons);
+    });
+  }, []);
+
+  console.log(countries);
   return (
     <div>
       <h1>Countries</h1>
+      <input></input>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ part2/countries/src/services/countries.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+ const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  console.log(newObject);
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+const erase = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+
+  return request.then((response) => response.statusText);
+};
+
+export default { getAll, create, update, erase };
