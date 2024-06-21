f30ea4b93d9ec148a6686676525e4886a4d54619
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-06-21 18:45:59 -0300

    search function created

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -4,18 +4,34 @@ import countryServices from "./services/countries";
 function App() {
   const [countries, setCountries] = useState([]);
   const [newSearchString, setNewSearchString] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   useEffect(() => {
     countryServices.getAll().then((initialCountries) => {
       setCountries(initialCountries);
     });
   }, []);
-  console.log(countries);
+  // console.log(countries);
 
   const handleSearchChange = (event) => {
     console.log(event.target.value);
     setNewSearchString(event.target.value);
   };
+  useEffect(()=> {
+    setFilteredCountries(countries.filter((country)=> {
+      const searchString = newSearchString.toLowerCase();
+      console.log(searchString);
+      const commonName = country.name.common.toLowerCase();
+      // console.log(commonName);
+      const officialName = country.name.official.toLowerCase();
+      
+      const regex = new RegExp(`\\b${searchString}\\w*\\b`, "i");
+      // console.log(regex.test(commonName));
+      
+      return regex.test(commonName) || regex.test(officialName);
+    }))
+  }, [newSearchString, setFilteredCountries, countries]);
+  console.log(filteredCountries.length)
 
   return (
     <div>
