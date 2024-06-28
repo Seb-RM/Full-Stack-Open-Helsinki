919c8430e9e350ed9949d48abab12ee17d397f0e
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-06-28 12:45:58 -0300

    country detail flag added

--- part2/countries/src/components/CountryDetail.jsx
+++ part2/countries/src/components/CountryDetail.jsx
@@ -1,16 +1,20 @@
 
 function CountryDetail({country}) {
     return (
-        <div>
-            <h1>{country.name.official}</h1>
-            <h5>{country.name.common}</h5>
-            <p>{country.capital}</p>
-            <p>{country.population}</p>
-            <p>{country.area}</p>
-            <ul>{Object.keys(country.languages).map(key => (
-                <li key={key}>{country.languages[key]}</li>
-            ))}</ul>
-        </div>
+      <div>
+        <h1>{country.name.official}</h1>
+        <h5>{country.name.common}</h5>
+        <p>{country.capital}</p>
+        <p>{country.population}</p>
+        <p>{country.area}</p>
+        <ul>
+          {Object.keys(country.languages).map((key) => (
+            <li key={key}>{country.languages[key]}</li>
+          ))}
+        </ul>
+        <p>{country.area}</p>
+        <img src={`${country.flags.svg}`} alt={`${country.flags.alt}`} />
+      </div>
     );
 }
 
