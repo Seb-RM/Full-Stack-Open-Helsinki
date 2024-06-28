a6f95fb185fbbed875d1d53d85fa7b1bfb02db4d
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-06-28 13:03:12 -0300

    weather api first configuration

--- part2/countries/src/components/CountryDetail.jsx
+++ part2/countries/src/components/CountryDetail.jsx
@@ -1,21 +1,22 @@
-
-function CountryDetail({country}) {
+import.meta.env; 
+function CountryDetail({ country }) {
+    console.log(import.meta.env.VITE_WEATHER_API);
     return (
-      <div>
+        <div>
         <h1>{country.name.official}</h1>
         <h5>{country.name.common}</h5>
         <p>{country.capital}</p>
         <p>{country.population}</p>
         <p>{country.area}</p>
         <ul>
-          {Object.keys(country.languages).map((key) => (
+            {Object.keys(country.languages).map((key) => (
             <li key={key}>{country.languages[key]}</li>
-          ))}
+            ))}
         </ul>
         <p>{country.area}</p>
         <img src={`${country.flags.svg}`} alt={`${country.flags.alt}`} />
-      </div>
+        </div>
     );
 }
 
-export default CountryDetail
+export default CountryDetail;
