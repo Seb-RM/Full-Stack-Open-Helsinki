6785fb686fdeaa2fb07be5114760a8d05cce2762
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-06-28 12:32:23 -0300

    Country Detail component firsts elements added

--- part2/countries/src/components/CountryDetail.jsx
+++ part2/countries/src/components/CountryDetail.jsx
@@ -1,10 +1,17 @@
 
 function CountryDetail({country}) {
     return (
-        <div>
-            <h1>{country.name.official}</h1>
-        </div>
-    )
+      <div>
+        <h1>{country.name.official}</h1>
+        <h5>{country.name.common}</h5>
+        <p>{country.capital}</p>
+        <p>{country.population}</p>
+        <p>{country.area}</p>
+        <ul>{Object.keys(country.languages).map(key => (
+            <li key={key}>{country.languages[key]}</li>
+        ))}</ul>
+      </div>
+    );
 }
 
 export default CountryDetail
