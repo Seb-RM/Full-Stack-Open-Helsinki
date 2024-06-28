d09870996f6a51e8be7f68ebbd5f4dc7670fc141
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-06-28 12:32:54 -0300

    minor correction

--- part2/countries/src/components/CountryDetail.jsx
+++ part2/countries/src/components/CountryDetail.jsx
@@ -1,16 +1,16 @@
 
 function CountryDetail({country}) {
     return (
-      <div>
-        <h1>{country.name.official}</h1>
-        <h5>{country.name.common}</h5>
-        <p>{country.capital}</p>
-        <p>{country.population}</p>
-        <p>{country.area}</p>
-        <ul>{Object.keys(country.languages).map(key => (
-            <li key={key}>{country.languages[key]}</li>
-        ))}</ul>
-      </div>
+        <div>
+            <h1>{country.name.official}</h1>
+            <h5>{country.name.common}</h5>
+            <p>{country.capital}</p>
+            <p>{country.population}</p>
+            <p>{country.area}</p>
+            <ul>{Object.keys(country.languages).map(key => (
+                <li key={key}>{country.languages[key]}</li>
+            ))}</ul>
+        </div>
     );
 }
 
