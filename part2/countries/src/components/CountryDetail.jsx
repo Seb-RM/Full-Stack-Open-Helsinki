0331edc11030d923dac262179a67c23acc18f01b
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-07-26 12:29:02 -0300

    conection with weather api

--- part2/countries/src/components/CountryDetail.jsx
+++ part2/countries/src/components/CountryDetail.jsx
@@ -1,6 +1,35 @@
-import.meta.env; 
+import.meta.env;
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function CountryDetail({ country }) {
     console.log(import.meta.env.VITE_WEATHER_API);
+    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}`;
+
+    const [weather, setWeather] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            if (country && country.capital) {
+                try {
+                const apiKey = "TU_API_KEY"; // Reemplaza con tu API key de OpenWeatherMap
+                const capital = country.capital[0]; // Asegúrate de que estás usando el nombre de la capital correcto
+                const response = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${import.meta.env.VITE_WEATHER_API}`
+                );
+                setWeather(response.data);
+                } catch (error) {
+                setError(error);
+            }
+        }
+        };
+
+        fetchWeather();
+    }, [country]);
+
+    console.log(weather)
+
     return (
         <div>
         <h1>{country.name.official}</h1>
