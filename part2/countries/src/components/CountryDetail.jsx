6c93e53aff1042df8b2c9af4dcacda14ad33709f
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-07-26 13:08:01 -0300

    visualization of weather data and icon

--- part2/countries/src/components/CountryDetail.jsx
+++ part2/countries/src/components/CountryDetail.jsx
@@ -4,23 +4,24 @@ import { useEffect, useState } from "react";
 
 function CountryDetail({ country }) {
     console.log(import.meta.env.VITE_WEATHER_API);
-    const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}`;
-
     const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchWeather = async () => {
             if (country && country.capital) {
                 try {
-                const apiKey = "TU_API_KEY"; // Reemplaza con tu API key de OpenWeatherMap
-                const capital = country.capital[0]; // Asegúrate de que estás usando el nombre de la capital correcto
+                const apiKey = import.meta.env.VITE_WEATHER_API; 
+                const capital = country.capital[0];
                 const response = await axios.get(
-                    `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${import.meta.env.VITE_WEATHER_API}`
+                    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}`
                 );
                 setWeather(response.data);
+                setLoading(false);
                 } catch (error) {
                 setError(error);
+                setLoading(false);
             }
         }
         };
@@ -28,23 +29,43 @@ function CountryDetail({ country }) {
         fetchWeather();
     }, [country]);
 
-    console.log(weather)
+    //console.log(weather)
+    const weatherIconUrl = weather
+        ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+        : "";
 
     return (
-        <div>
-        <h1>{country.name.official}</h1>
-        <h5>{country.name.common}</h5>
-        <p>{country.capital}</p>
-        <p>{country.population}</p>
-        <p>{country.area}</p>
-        <ul>
-            {Object.keys(country.languages).map((key) => (
-            <li key={key}>{country.languages[key]}</li>
-            ))}
-        </ul>
-        <p>{country.area}</p>
-        <img src={`${country.flags.svg}`} alt={`${country.flags.alt}`} />
-        </div>
+        <>
+            <div>
+            <h1>{country.name.official}</h1>
+            <h5>{country.name.common}</h5>
+            <p>{country.capital}</p>
+            <p>{country.population}</p>
+            <p>{country.area}</p>
+            <ul>
+                {Object.keys(country.languages).map((key) => (
+                <li key={key}>{country.languages[key]}</li>
+                ))}
+            </ul>
+            <p>{country.area}</p>
+            <img src={`${country.flags.svg}`} alt={`${country.flags.alt}`} />
+            </div>
+            <div>
+                {loading ? (
+                <p>Loading weather...</p>
+                    ) : error ? (
+                <p>Error fetching weather data: {error.message}</p>
+                    ) : (
+                <div>
+                <h3>Weather in {country.capital}:</h3>
+                <p>Temperature: {weather.main.temp}K</p>
+                <p>Weather: {weather.weather[0].description}</p>
+                <p>Wind Speed: {weather.wind.speed} m/s</p>
+                <img src={weatherIconUrl} alt="Weather icon" />
+                </div>
+            )}
+            </div>
+        </>
     );
 }
 
