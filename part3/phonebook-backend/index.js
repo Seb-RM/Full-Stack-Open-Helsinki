7badc525632032d45da158bb9f56f48b78050247
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-08-09 17:52:57 -0300

    morgan predefined format added

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -30,6 +30,14 @@ let data = [
 
 app.use(express.json());
 
+morgan.token("body", (req) => {
+    return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
+app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
