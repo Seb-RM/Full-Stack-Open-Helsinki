ec7f806bf71cce6017fbb227486f9065b3cec1b2
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-08-09 11:49:00 -0300

    route persons/:id created and working

--- part3/index.js
+++ part3/index.js
@@ -45,6 +45,17 @@ app.get("/info", (request, response) => {
         `);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = data.find((person) => person.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).send({ error: "Person not found" });
+    }
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
